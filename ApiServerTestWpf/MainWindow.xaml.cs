aec48cea33944b56dc1a1ca96a7707b4dc224535
Ilya <166903407+dekech111@users.noreply.github.com>
2024-04-13 11:14:54 +0300

    req

---
+++ ApiServerTestWpf/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Windows;
+
+namespace ApiServerTestWpf
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            tbUrlApi.Text = "https://localhost:44369/UploadFiles";
+        }
+
+        private void btnStartUpload_Click(object sender, RoutedEventArgs e)
+        {
+
+            var url = tbUrlApi.Text;
+            Uri uri = null;
+            if (string.IsNullOrEmpty(url) && !Uri.TryCreate(url, UriKind.Absolute, out uri))
+            {
+                return;
+            }
+
+
+            var dlg = new OpenFileDialog { CheckFileExists = true, InitialDirectory = Environment.CurrentDirectory};
+
+                if (dlg.ShowDialog() == true && !string.IsNullOrEmpty(dlg.FileName))
+                {
+                    var fileName = dlg.FileName;
+                try
+                {
+                    using (HttpClientHandler hdl = new HttpClientHandler())
+                        {
+                            using (HttpClient clnt = new HttpClient(hdl))
+                            {
+                                using (MultipartFormDataContent content = new MultipartFormDataContent())
+                                {
+                                    using (var fileContent = new ByteArrayContent(File.ReadAllBytes(fileName)))
+                                    {
+                                        fileContent.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment")
+                                        {
+                                            FileName = Path.GetFileName(fileName),
+                                            Name = "file"
+                                        };
+
+                                        content.Add(fileContent);
+
+                                        var response = clnt.PostAsync(url, content).Result;
+
+                                        var text = response.Content.ReadAsStringAsync().Result;
+                                        MessageBox.Show(text);
+                                    }
+                                }
+                            }
+                    }
+                }
+                catch (Exception ex) { MessageBox.Show(ex.Message); }
+                }
+
+            
+            
+        }
+    }
+}
---
+++ WebApplication2/Controllers/DefaultController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace WebApplication2.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        [AcceptVerbs("GET")]
+        [Route("Ping/{id}")]
+        public async Task<IHttpActionResult> Ping(int id)
+        {
+            return Ok(new {Id = id, Имя = "Илья", Фамилия = "Шадрин"});
+        }
+
+        [AcceptVerbs("POST")]
+        [Route("PostData")]
+        public async Task<IHttpActionResult> PostData([FromBody] string[] data)
+        {
+            return Ok(new { date = DateTime.Now, data });
+        }
+
+        [AcceptVerbs("POST")]
+        [Route("UploadFiles")]
+        public async Task<IHttpActionResult> UploadFiles()
+        {
+            try
+            {
+                MultipartMemoryStreamProvider provider = new MultipartMemoryStreamProvider();
+                await Request.Content.ReadAsMultipartAsync(provider);
+
+                if(provider.Contents != null && provider.Contents.Count > 0)
+                {
+                    foreach (var cont in provider.Contents)
+                    {
+                        if (!string.IsNullOrEmpty(cont.Headers.ContentDisposition.FileName))
+                        {
+                            var filename = cont.Headers.ContentDisposition.FileName;
+
+                            var saveDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Files");
+                            if (!Directory.Exists(saveDir))
+                            {
+                                Directory.CreateDirectory(saveDir);
+                            }
+
+                            var filePath = Path.Combine(saveDir, filename);
+                            if(File.Exists(filePath))
+                            {
+                                File.Delete(filePath);
+                            }
+
+                            var bites = await cont.ReadAsByteArrayAsync();
+                            if(bites != null && bites.Length > 0)
+                            {
+                                using (var streamMemory = new MemoryStream(bites))
+                                {
+                                    streamMemory.Seek(0, SeekOrigin.Begin);
+                                    using (var fileStream = File.Create(filePath))
+                                    {
+                                        streamMemory.CopyTo(fileStream);
+                                    }
+                                }
+                            }
+                            else { return Ok(new { message = "Файл пустой!" }); }
+                        }
+                        else { return Ok(new { message = "Файл без имени!" }); }
+                    }
+
+                    return Ok(new { message = "Успех" });
+                }
+                else { return Ok(new { message = "Файл не найден" }); }
+            }
+            catch (Exception ex) { return InternalServerError(ex); }
+        }
+    }
+}
